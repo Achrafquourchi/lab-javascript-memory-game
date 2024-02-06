f22f2d143291a3ede5721557667023674cbf8505
Achrafquourchi <quourchiachraf@gmail.com>
2024-02-06 18:57:28 +0100

    lab done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,45 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) return undefined;
+    let shuffledDeck = [];
+    let randNums = [];
+    let currRandNum;
+    this.cards.forEach((element) => {
+      currRandNum = this.getRandomInt(this.cards.length);
+      while (randNums.includes(currRandNum)) {
+        currRandNum = this.getRandomInt(this.cards.length);
+      }
+      randNums.push(currRandNum);
+      shuffledDeck.push(this.cards[currRandNum]);
+    });
+    this.cards = [...shuffledDeck];
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    let result = false;
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      result = true;
+    }
+    return result;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    let isFinished = false;
+    if (this.pairsGuessed === this.cards.length / 2) {
+      isFinished = true;
+    }
+    return isFinished;
   }
 }
+
+// lab done
